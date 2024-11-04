cf82a729483594b5c85c5c96d6ac902ca026a562
Mbasa Cokile <mbasacokile7@gmail.com>
2024-11-04 20:59:04 +0200

    Loop through 5-Day/3-hour data to get temp readings for each day: NOT FINISHED

--- index.js
+++ index.js
@@ -40,6 +40,48 @@ app.get("/return", (req, res) =>{
   res.redirect("/");
 });
 
+// Create an object to store all the average temp values for each day
+const averageTemps = {dayOne: [], dayTwo: [], dayThree: [], dayFour: [], dayFive:[]};
+// Create a function to extract certain data for 5-day forecast
+let count = 0;
+function dataRetrieval(data){
+  // Loop through the data arrray(data.List)
+  
+  for (let item in data){
+    count++
+    //Get the date of each data entry/timestamp
+    const dateTime = new Date(data[item].dt_txt);
+    //Get the specific date
+    const date = dateTime.getDate();
+    //console.log(date);
+    // Use Switch Statement to execute certain codeblocks
+    switch (new Date(data[item].dt_txt).getDate()) {
+      case (date):
+        console.log("Day One")
+        averageTemps.dayOne.push(data[item].main.temp);
+        break;
+      case (date + 1):
+        console.log("Day Two")
+        averageTemps.dayTwo.push(data[item].main.temp);
+        break;
+      case date + 2:
+        console.log("Day Three")
+        averageTemps.dayThree.push(data[item].main.temp);
+        break;
+      case (date + 3):
+        console.log("Day Four")
+        averageTemps.dayFour.push(data[item].main.temp);
+        break;
+      case (date + 4):
+        console.log("Day Five")
+        averageTemps.dayFive.push(data[item].main.temp);
+        break;
+    }
+  }
+  console.log(count)
+ return averageTemps
+}
+
 //Post request to view response from API call
 app.post("/results", async (req, res) => {
   // Get data that the user entered using body-parser
@@ -49,7 +91,7 @@ app.post("/results", async (req, res) => {
   try {
     // API URL to get Current Weather forecast
     const apiURL = "https://api.openweathermap.org/data/2.5/weather?q=" + userCity + "," + userCountryCode +"&units=metric&appid=" + apiKey;
-    // API URL for 5 Day forecast
+    // API URL for 7 Day forecast
     const apiURL_2 = "https://api.openweathermap.org/data/2.5/forecast?q=" + userCity + "," + userCountryCode + "&units=metric&appid=" + apiKey;
     // Use Promise.all() to make parallel requests, to make the app fast
     const [response, response_2] = await Promise.all([
@@ -63,7 +105,9 @@ app.post("/results", async (req, res) => {
    
     //Store 5 Day Forecast data
     const result_2 = response_2.data
-    console.log(result_2);
+    //Apply data retrieval function to get desired temps
+    const dailyTemps = dataRetrieval(result_2.list);
+    //console.log(dailyTemps);
     //Render results ejs page and convert the JS Object into a string
     res.render("results.ejs", {data: result, iconURL:iconURL});
   } catch (error) {
